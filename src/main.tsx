be5ac2c94130680fde620bbc18d0b8b8f9874891
Ujjwal Singh <ujjusingh20@gmail.com>
2026-04-16 23:06:40 +0530

    added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 import './index.css'
 import App from './App.tsx'
@@ -7,6 +8,7 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
     <>
         <App />
+        <Analytics />
         <SpeedInsights />
     </>
 )
